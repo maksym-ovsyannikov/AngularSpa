79cbad879312ab5ad0b98bdc00d990194dfcbcbf
Maksym Ovsyannikov <Maksym_Ovsyannikov@epam.com>
2018-03-06 18:06:34 +0200

    added search, http

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { AppRoutingModule } from ".//app-routing.module";
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
-import { HeroService } from "./service/hero.service";
 import { MessagesComponent } from "./messages/messages.component";
-import { MessageService } from "./service/message.service";
-import { AppRoutingModule } from ".//app-routing.module";
 import { DashboardComponent } from './dashboard/dashboard.component';
- 
+
+import { MessageService } from "./service/message.service";
+import { HeroService } from "./service/hero.service";
+import { InMemoryDataService } from './service/in-memory-data.service';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
+
+
+
 
 @NgModule({
   declarations: [
@@ -18,12 +25,18 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
+
   ],
   providers: [HeroService, MessageService],
   bootstrap: [AppComponent]
--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 //import Hero = require("../entities/hero");
 import { Hero } from "../entities/hero";
-import { HEROES } from '../mocks/mock-heroes';
 import { HeroService } from '../service/hero.service';
 
 @Component({
@@ -45,5 +44,19 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes()
                     .subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
   }
 }
